b3efb4714b042a6d508caf6429188586f8769000
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-02-20 11:15:25 +0000

    Create posts model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
+"""blog model"""
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Post(models.Model):
+    """class of Posts Model for blog"""
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    class Meta:
+        """created on field"""
+        oredering = ['-created_on']
+
+    def __str__(self):
+        """return self title"""
+        return self.title
+
+    def number_of_likes(self):
+        """number of likes in a post"""
+        return self.likes.count()
