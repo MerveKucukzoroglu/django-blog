eac95b94497e2c49c21d814d8365efcff03d4430
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-02-24 09:31:18 +0000

    Create view post and comments code

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 """views file"""
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post
 
 
@@ -10,3 +10,26 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class PostDetail(View):
+    """Class to view post details"""
+
+    def get(self, request, slug, *args, **kwargs):
+        """def"""
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            },
+        )
