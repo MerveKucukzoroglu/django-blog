1c5233b6d914cb41f0ce4596a9b173ebba061d14
MerveKucukzoroglu <mkucukzoroglu@gmail.com>
2022-02-24 07:55:20 +0000

    Create the view class

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
+"""views file"""
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+
+class PostList(generic.ListView):
+    """List Views class"""
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+    paginate_by = 6
